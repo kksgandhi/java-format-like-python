e3ab106df0c45aad026f384dbc87d73e59d7fb0f
Kutub Gandhi <kksgandhi@gmail.com>
2018-07-03 14:46:18 -0700

    Hello World commit

---
+++ formatter.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    print(filename)
---
+++ test_file.java
@@ -0,0 +1,21 @@
+public class test_file{
+
+    public static void main(String[] args) {
+        // This is a comment!
+        
+        for(int i = 0; i < 3; i++){
+
+            System.out.println("Hello World");
+
+        }
+        if (true){
+            if(false) {
+                System.out.println("Hello World");
+            }
+            else {
+                System.out.println("Hello World");
+            }
+        }
+    }
+}
+
